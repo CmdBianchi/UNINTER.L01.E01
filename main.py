ba587c309efd07b9fb7b1197d6ea46d4a9da946a
marcus bianchi <marcusbianchi92@hotmail.com>
2021-07-02 11:37:10 -0300

    Commit 02

--- main.py
+++ main.py
@@ -3,19 +3,22 @@
 
 x = float(input('Inserir dados ? 0 - Não && 1 - SIM \nDIGITE: '))
 while (x == 1):
-    nome = input('Digite o nome do aluno: ')
-    nota = float(input('Digite a nota do aluno: '))
+    name = input('Digite o nome do aluno: ')
+    grade = float(input('Digite a nota do aluno: '))
 
-    if(nota <= 2.9):
+    if(grade <= 2.9):
         status = 'E'
-    elif(nota > 2.9) and (nota <= 4.9 ):
+    elif(grade > 2.9) and (grade <= 4.9 ):
         status = 'D'
-    elif (nota > 5.0) and (nota <= 6.9):
+    elif (grade > 5.0) and (grade <= 6.9):
         status = 'C'
-    elif (nota > 7.0) and (nota <= 8.9):
+    elif (grade > 7.0) and (grade <= 8.9):
         status = 'B'
     else:
         status = 'A'
-
-    print('Nota {}, Nome {},  Conceito {}.'.format(nota, nome, status))
+    print('-----------------------------------------------')
+    print('O aluno {}, tirou a Nota {} e com o conceito {}.'.format(name, grade, status))
+    print('-----------------------------------------------')
     x = float(input('Inserir dados ? 0 - Não && 1 - SIM \nDIGITE: '))
+    print('')
+print('PROGRAMA ENCERRADO......')
